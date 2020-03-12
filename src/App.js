c536d9201a48de21dc3ef7744a0970000eedebe2
ivayloyovkov <ivayloyovkovofficial@gmail.com>
2020-03-12 20:54:54 +0200

    added styled sign-in component, form component for repeatable forms and a custom button component to use where is needed

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header.component/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
