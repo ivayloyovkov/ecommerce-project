92469948b289ff7e7c79098366498504996feb2b
ivayloyovkov <ivayloyovkovofficial@gmail.com>
2020-03-15 04:49:26 +0200

    fully integrated and tested the firebase sign in

--- src/App.js
+++ src/App.js
@@ -17,16 +17,22 @@ class App extends React.Component {
     };
   }
 
+  unsubscribeFromAuth = null;
+
   componentDidMount() {
-    auth.onAuthStateChanged(user => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
       this.setState({ currentUser: user });
     });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
   }
 
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
--- src/components/header.component/header.component.jsx
+++ src/components/header.component/header.component.jsx
@@ -1,22 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { auth } from "../../firebase/firebase.utils";
+
 import { ReactComponent as Logo } from "../../assets/original.svg";
 
 import "./header.component.styles.scss";
 
-const Header = () => (
+const Header = ({ currentUser }) => (
   <div className="header">
     <Link to="/" className="logo-container">
       <Logo className="logo" />
     </Link>
     <div className="nav-container">
-        <Link to='/shop' className='nav-option'>
-            SHOP
+      <Link to="/shop" className="nav-option">
+        SHOP
+      </Link>
+      <Link to="/contact" className="nav-option">
+        CONTACT
+      </Link>
+      {currentUser ? (
+        <div className="nav-option" onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className="nav-option" to="/signin">
+          SIGN IN
         </Link>
-        <Link to='/contact' className='nav-option'>
-            CONTACT
-        </Link>
+      )}
     </div>
   </div>
 );
