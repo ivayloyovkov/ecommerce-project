efcc1f3a23257eb8757fad59f95475361a7239a0
Ivaylo Yovkov <ivayloyovkovofficial@gmail.com>
2020-03-23 16:07:57 +0200

    created redux directory and shop to organize shop/directory data

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,19 @@
 import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectShopCollections } from "../../redux/shop/shop.selectors";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
+const mapStateToProps = createStructuredSelector({
+  collections: selectShopCollections
+});
 
-    this.state = {
-        collections: SHOP_DATA
-    }
-  }
-  render(){
-    const {collections} = this.state;
-      return (
-        <div className='shop-page'>
-          {
-            collections.map(({ id, ...otherCollectionProps }) => (
-              <CollectionPreview key={id} {...otherCollectionProps} />
-            ))
-          }
-        </div>
-      )
-  }
-}
-
-export default ShopPage;
+export default connect(mapStateToProps) (ShopPage);
