8305587b7915ddbae405715476c6c4fefc599708
Ivaylo Yovkov <ivayloyovkovofficial@gmail.com>
2020-03-24 19:05:48 +0200

    move shop and directory data into their own reducers

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,19 +1,15 @@
 import React from "react";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
-import { selectShopCollections } from "../../redux/shop/shop.selectors";
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import { Route } from 'react-router-dom';
 
-const ShopPage = ({ collections }) => (
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from "../collection/collection.component";
+
+const ShopPage = ({ match }) => (
   <div className="shop-page">
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectShopCollections
-});
 
-export default connect(mapStateToProps) (ShopPage);
+export default ShopPage;
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -3,6 +3,17 @@ import { createSelector } from "reselect";
 const selectShop = state => state.shop;
 
 export const selectShopCollections = createSelector(
-         [selectShop],
-         shop => shop.collections
-       );
+  [selectShop],
+  shop => shop.collections
+);
+
+export const selectCollectionsForPreview = createSelector(
+  [selectShopCollections],
+  collections => Object.keys(collections).map(key => collections[key])
+);
+
+export const selectCollection = collectionUrlParam =>
+  createSelector(
+    [selectShopCollections],
+    collections => collections[collectionUrlParam]
+  );
