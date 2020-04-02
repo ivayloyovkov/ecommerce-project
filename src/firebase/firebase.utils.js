ea6a0c88aca3721aab3f99edf8a35b6bd7233e3d
Ivaylo Yovkov <ivayloyovkovofficial@gmail.com>
2020-04-02 17:34:03 +0300

    updated shop component to get the data from firestore and created shop types and shop actions

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -20,22 +20,54 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
   const snapShot = await userRef.get();
 
-  if(!snapShot.exists){
+  if (!snapShot.exists) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
 
-    try{
+    try {
       await userRef.set({
         displayName,
         email,
         createdAt,
         ...additionalData
-      })
-    } catch(error) {
-      console.log('error creating user', error.message)
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
     }
   }
   return userRef;
+};
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
 };
 
 firebase.initializeApp(config);
